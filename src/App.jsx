ac671e06b875d09ceea551bf25131fc5f1b5a05b
Rompa1992 <n.a.rompa@gmail.com>
2025-03-18 22:19:21 +1100

    Start Section 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
 import { useState } from 'react'; // React 
 
 import { CORE_CONCEPTS } from './data.js';
+import { EXAMPLES } from './data.js';
 import Header from './components/header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
   // React hook that allows you to add state to a functional component.
-  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button'); 
+  const [ selectedTopic, setSelectedTopic ] = useState(); 
 
   // selectedButton => 'Components', 'JSX', 'Props', or 'State'.
   function handleSelect(selectedButton) {
-    tabContent = selectedButton;
+    setSelectedTopic(selectedButton);
     console.log('selectedButton:', selectedButton);
   }
 
+  // tabContent: under the 'Examples' section, the content that appears when a tab is selected.
+  let tabContent = <p>Please select a topic.</p>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>{EXAMPLES[selectedTopic].code}</code>
+      </pre>
+    </div>
+    );
+  }
 
   return (
     <div>  
@@ -23,16 +37,20 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept 
+            {CORE_CONCEPTS.map((conceptItem) => ( 
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
+            {/* OLD WAY */}
+            {/*<CoreConcept 
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image} 
             />
-            {/* This notation is called the "spread operator" and is used to pass all the properties of an object as props to a component. */}
-            {/* This is equivalent to passing each property individually, as seen in the first CoreConcept component (element 0). */}
+            // This notation is called the "spread operator" and is used to pass all the properties of an object as props to a component. 
+            // This is equivalent to passing each property individually, as seen in the first CoreConcept component (element 0). 
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
           </ul>
         </section>
         <section id="examples">
@@ -43,14 +61,18 @@ function App() {
                 The onSelect prop receives an arrow function that calls handleSelect with 'components' as an argument.
                 This ensures handleSelect is called with the correct parameter when the button is clicked.
             */}
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'components'} 
+                       onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} 
+                       onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'}
+                       onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} 
+                       onSelect={() => handleSelect('state')}>State</TabButton>
           </menu> 
           {tabContent}
         </section>
-      </main>g
+      </main>
     </div>
   );
 }
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,7 @@
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
     return (
     <li>    
-        <button onClick={onSelect}>{children}</button> {/* The children prop is used to access the content between the opening and closing tags of the component. */}
+        <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button> {/* The children prop is used to access the content between the opening and closing tags of the component. */}
     </li>
     ); 
 }
